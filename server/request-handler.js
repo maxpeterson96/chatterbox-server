72ddd748a1249508bf56d3e5ecc1d5792862ce7f
Maxwell Peterson <mpeterson94@me.com>
2022-11-12 12:10:50 -0800

    progress

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,32 +12,50 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
-const storage = ['empty array'];
+const storage = [];
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept, authorization',
+  'access-control-max-age': 10 // Seconds.
+};
+
+//const header = {'Content-Type', 'application/json'};
 
 var requestHandler = function(request, response) {
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-  console.log(request);
+  // console.log(request);
   //http://127.0.0.1:3000/classes/messages
-  if (request.method === 'GET') {
-    if (request.url === "/classes/messages") {
+
+  if (request.url === "/classes/messages") {
+    if (request.method === 'GET') {
       var statusCode = 200;
       var headers = defaultCorsHeaders;
-      headers['Content-Type'] = 'text/plain';
+      response.setHeader('Content-Type', 'application/json');
       response.writeHead(statusCode, headers);
       response.end(JSON.stringify(storage));
+    } else if (request.method === 'POST') {
+      let body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        response.setHeader('Content-Type', 'application/json');
+        response.statusCode = 201;
+
+        // response.write(JSON.stringify(responseBody));
+        storage.push(JSON.parse(body));
+        response.end(JSON.stringify(storage));
+
+        //console.log('body after = ', body);
+        // at this point, `body` has the entire request body stored in it as a string
+      });
     }
-    if (request.url === "/japanese/sushi") {
-      var statusCode = 200;
-      var headers = defaultCorsHeaders;
-      headers['Content-Type'] = 'text/plain';
-      response.writeHead(statusCode, headers);
-      response.end('sushi');
-    }
-  } else if (request.method === 'POST') {
-    if (request.url === "/classes/messages") {
-      console.log('post success');
-    }
+  } else {
+    response.statusCode = 404;
+    response.end();
   }
 };
 
@@ -52,9 +70,7 @@ exports.handleRequest = requestHandler;
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept, authorization',
-  'access-control-max-age': 10 // Seconds.
-};
+
+// let temp3 = JSON.stringify(body);
+//         console.log('parse = ', JSON.parse(temp3));
+//         console.log('stringify = ', JSON.stringify(body));
